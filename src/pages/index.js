a974c8fa316ff75dd296c17ef7b93137b0f658a6
Edgar Talledos <onimenotsuki@gmail.com>
2018-12-07 00:30:57 -0600

    Fetch articles succesfuly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <div className="w-100 pa4">
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-    </div>
-  </Layout>
-)
+const mediumLink = (username, uniqueSlug) => {
+  const url = 'https://medium.com';
+  const permalink = encodeURIComponent(uniqueSlug);
+
+  return `${url}/${username}/${permalink}`;
+}
+
+class IndexPage extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      payload: {
+        posts: [],
+      },
+    }
+  }
+
+  componentDidMount() {
+    const username = 'edgar-talledos'
+
+    fetch(`${process.env.API_URL}?username=${username}`)
+      .then(res => res.json())
+      .then(({ payload }) => this.setState({ payload }))
+  }
+
+  componentWillUnmount() {
+    this.setState({ payload: { posts: [] } })
+  }
+
+  render() {
+    const { payload: { posts } } = this.state;
+
+    return (
+      <Layout>
+        {posts.map(post => (
+          <div
+            key={post.id}
+            className="fl w-100 w-50-ns pa4"
+          >
+            <h1 className="f2 lh-title">{post.title}</h1>
+            <p className="f3 lh-copy">{post.virtuals.subtitle}</p>
+            <a
+              className="link black dim"
+              href={mediumLink('edgar-talledos', post.uniqueSlug)}
+            >
+              Leer más...
+            </a>
+          </div>
+        ))}
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
